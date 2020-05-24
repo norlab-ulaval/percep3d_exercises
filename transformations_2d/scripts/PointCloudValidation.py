194b6973ce99dff8aa42d67378b38eca78f42c19
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2020-05-23 23:45:13 -0400

    first commit

---
+++ transformations_2d/scripts/PointCloudValidation.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+import numpy as np
+from matplotlib import animation
+
+from scripts.helper_func import *
+from scripts.simulator import *
+from scripts.SnakeStaticValidation import *
+
+class PointCloudValidation(SnakeStaticValidation):
+    def __init__(self, n):
+        super().__init__()
+        self.__pt_eye_alpha_left = np.empty((3))*np.nan
+        self.__pt_eye_alpha_right = np.empty((3))*np.nan
+        self.__pt_eye_beta_left = np.empty((3))*np.nan
+        self.__pt_eye_beta_right = np.empty((3))*np.nan
+        self.__time = 0
+        self.nb_frames = n
+        
+        # snake parts
+        self.head_parts_l = generate_snake_head()
+        self.head_parts_r = generate_snake_head()
+        self.body_parts = generate_snake_body()
+        self.tail_parts = generate_snake_tail()
+        self.rattle_parts = generate_snake_rattle()
+        self.eye_parts_1 = generate_snake_eye()
+        self.eye_parts_2 = generate_snake_eye()
+        self.eye_parts_3 = generate_snake_eye()
+        self.eye_parts_4 = generate_snake_eye()
+
+        # prepare handles for animation
+        self.fig, self.ax = plt.subplots(nrows=1, ncols=1, figsize=(10,10))
+        self.ploly_snake_body = draw_snake_body(self.ax, self.body_parts, np.eye(3))
+        self.ploly_snake_headl, self.ploly_snake_neckl = draw_snake_head(self.ax, self.head_parts_l, np.eye(3))
+        self.ploly_snake_headr, self.ploly_snake_neckr = draw_snake_head(self.ax, self.head_parts_r, np.eye(3))
+
+        self.ploly_snake_tail1, self.ploly_snake_tail2 = draw_snake_tail(self.ax, self.tail_parts, np.eye(3))
+        self.ploly_snake_rattle = draw_snake_rattle(self.ax, self.rattle_parts, np.eye(3))
+
+        self.ploly_snake_eye1 = draw_snake_eye(self.ax, self.eye_parts_1, np.eye(3))
+        self.ploly_snake_eye2 = draw_snake_eye(self.ax, self.eye_parts_2, np.eye(3))
+        self.ploly_snake_eye3 = draw_snake_eye(self.ax, self.eye_parts_3, np.eye(3))
+        self.ploly_snake_eye4 = draw_snake_eye(self.ax, self.eye_parts_4, np.eye(3))
+
+        self.map_scatter = self.ax.scatter([], [], c="tab:red", s=4)
+        self.point_cloud = np.array([]).reshape(0,3)
+        
+        # list of all plotting data
+        self.all_body = []
+        self.all_headl = []
+        self.all_neckl = []
+        self.all_headr = []
+        self.all_neckr = []
+
+        self.all_tail1 = []
+        self.all_tail2 = []
+        self.all_rattle = []
+
+        self.all_eye1 = []
+        self.all_eye2 = []
+        self.all_eye3 = []
+        self.all_eye4 = []
+        
+        self.all_pt_eye_alpha_left = []
+        self.all_pt_eye_alpha_right = []
+        self.all_pt_eye_beta_left = []
+        self.all_pt_eye_beta_right = []
+
+    def valid_point(self, point):
+        assert(point.shape[0] == 3 and point.ndim == 1), "The point must have 3 elements (homogeneous coordiantes)!"
+        return point
+    
+    def set_global_point_alpha_left(self, point): 
+        self.__pt_eye_alpha_left = self.valid_point(point)
+    
+    def set_global_point_alpha_right(self, point):
+        self.__pt_eye_alpha_right = self.valid_point(point)
+        
+    def set_global_point_beta_left(self, point):
+        self.__pt_eye_beta_left = self.valid_point(point)
+        
+    def set_global_point_beta_right(self, point):
+        self.__pt_eye_beta_right = self.valid_point(point)
+
+    def save_animation_frame(self):
+        self.all_body.append((self._SnakeStaticValidation__body @ self.body_parts[1])[0:2,:].T)
+        self.all_headl.append((self._SnakeStaticValidation__head_alpha @ self.head_parts_l[1])[0:2,:].T)
+        self.all_neckl.append((self._SnakeStaticValidation__head_alpha @ self.head_parts_l[2])[0:2,:].T)
+        self.all_headr.append((self._SnakeStaticValidation__head_beta @ self.head_parts_r[1])[0:2,:].T)
+        self.all_neckr.append((self._SnakeStaticValidation__head_beta @ self.head_parts_r[2])[0:2,:].T)
+
+        self.all_tail1.append((self._SnakeStaticValidation__tail @ self.tail_parts[1])[0:2,:].T)
+        self.all_tail2.append((self._SnakeStaticValidation__tail @ self.tail_parts[2])[0:2,:].T)
+        self.all_rattle.append((self._SnakeStaticValidation__rattle @ self.rattle_parts[1])[0:2,:].T)
+
+        self.all_eye1.append((self._SnakeStaticValidation__eye_alpha_left @ self.eye_parts_1[1])[0:2,:].T)
+        self.all_eye2.append((self._SnakeStaticValidation__eye_alpha_right @ self.eye_parts_2[1])[0:2,:].T)
+        self.all_eye3.append((self._SnakeStaticValidation__eye_beta_left @ self.eye_parts_3[1])[0:2,:].T)
+        self.all_eye4.append((self._SnakeStaticValidation__eye_beta_right @ self.eye_parts_4[1])[0:2,:].T)
+        
+        self.all_pt_eye_alpha_left.append(self.__pt_eye_alpha_left)
+        self.all_pt_eye_alpha_right.append(self.__pt_eye_alpha_right)
+        self.all_pt_eye_beta_left.append(self.__pt_eye_beta_left)
+        self.all_pt_eye_beta_right.append(self.__pt_eye_alpha_left)
+        
+    def draw_frame(self, t):
+        self.ploly_snake_body.set_xy(self.all_body[t])
+        self.ploly_snake_headl.set_xy(self.all_headl[t])
+        self.ploly_snake_neckl.set_xy(self.all_neckl[t])
+        self.ploly_snake_headr.set_xy(self.all_headr[t])
+        self.ploly_snake_neckr.set_xy(self.all_neckr[t])
+
+        self.ploly_snake_tail1.set_xy(self.all_tail1[t])
+        self.ploly_snake_tail2.set_xy(self.all_tail2[t])
+        self.ploly_snake_rattle.set_xy(self.all_rattle[t])
+
+        self.ploly_snake_eye1.set_xy(self.all_eye1[t])
+        self.ploly_snake_eye2.set_xy(self.all_eye2[t])
+        self.ploly_snake_eye3.set_xy(self.all_eye3[t])
+        self.ploly_snake_eye4.set_xy(self.all_eye4[t])
+        
+        self.point_cloud = np.vstack([self.point_cloud, self.all_pt_eye_alpha_left[t], self.all_pt_eye_alpha_right[t],
+                                      self.all_pt_eye_beta_right[t], self.all_pt_eye_beta_left[t]])
+        
+        self.map_scatter.set_offsets(self.point_cloud[:,0:2])
+            
+        self.ax.set_xlim((-50, 50))
+        self.ax.set_ylim((-50, 50))
+        
+        return (self.ploly_snake_body,
+                self.ploly_snake_headl, 
+                self.ploly_snake_neckl, 
+                self.ploly_snake_headr, 
+                self.ploly_snake_neckr,
+                self.ploly_snake_tail1,
+                self.ploly_snake_tail2,
+                self.ploly_snake_rattle,
+                self.ploly_snake_eye1,
+                self.ploly_snake_eye2,
+                self.ploly_snake_eye4
+               )
+    
+    def display_all_frames(self):
+        for t in np.arange(self.nb_frames):
+            self.draw_frame(t)
+        self.point_cloud = np.array([]).reshape(0,3)
+        
+    def display_animation(self):
+        anim = animation.FuncAnimation(self.fig, self.draw_frame,
+                               frames=np.arange(self.nb_frames), interval=40, 
+                               blit=True, repeat=False)
+        plt.close(anim._fig)
+        return anim
---
+++ transformations_2d/scripts/simulator.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+
+import numpy as np
+from scripts.helper_func import *
+
+S_points = np.array([[0., 3, 5, 5, 1.,  1,  3,  5,  5,  2,  0, 0, 4, 4, 2, 0, 0],
+                     [0., 0, 2, 5, 9., 10, 12, 12, 13, 13, 11, 8, 4, 3, 1, 1, 0],
+                     [1., 1, 1, 1, 1.,  1,  1,  1,  1,  1,  1, 1, 1, 1, 1, 1, 1]])
+
+E_points = np.array([[0., 5, 5, 1, 1., 3, 3, 1,  1,  5,  5,  0, 0],
+                     [0., 0, 1, 1, 6., 6, 7, 7, 12, 12, 13, 13, 0],
+                     [1., 1, 1, 1, 1., 1, 1, 1,  1,  1,  1,  1, 1]])
+
+two_points = np.array([[0., 5, 5, 1, 5.,  5,  3,  0,  0,  2,  4, 4, 0, 0],
+                       [0., 0, 1, 1, 5., 11, 13, 13, 12, 12, 10, 6, 2, 0],
+                       [1., 1, 1, 1, 1.,  1,  1,  1,  1,  1,  1, 1, 1, 1]])
+
+square_points = np.array([[1, 1, -1, -1, 1],
+                          [-1, 1, 1, -1, -1],
+                          [1, 1, 1, 1, 1]])
+
+def ray_tracing_single_shape(T, array_points):
+    
+    
+    #ray = ray_origin - ray_head
+    
+    #T = rigid_tranformation((ray_origin[0], ray_origin[1], ray_angle))
+    L = np.linalg.inv(T) @ array_points
+    
+    #ax.plot(L[0], L[1]);
+    
+    intersec = np.empty((L.shape[0],L.shape[1]-1))
+    intersec[2,:] = np.ones(L.shape[1]-1)
+
+    for i in np.arange(L.shape[1]-1):
+
+        dx = L[0,i] - L[0,i+1]
+        dy = L[1,i] - L[1,i+1]
+        if(dy != 0):
+            intersec[0,i] = L[0,i] - (dx/dy)*L[1,i]
+        else:
+            intersec[0,i] = L[0,i]
+        intersec[1,i] = 0
+        
+        # is the point within the segment
+        bound_max = np.max([L[0,i], L[0,i+1]])
+        bound_min = np.min([L[0,i], L[0,i+1]])
+
+        outside = not((intersec[0,i] <= bound_max) and 
+                  (intersec[0,i] >= bound_min))
+
+        behind = (intersec[0,i] < 0)
+        if(outside or behind):
+            intersec[0:2,i] = [np.nan, np.nan]
+
+    closest_id = 0
+    if(np.isfinite(intersec[0,:]).any()):
+        closest_id = np.nanargmin(intersec[0,:])
+    
+    if(np.isfinite(intersec[:,closest_id]).all()):
+        closest_hit = T @ intersec[:,closest_id]
+    else:
+        closest_hit = intersec[:,closest_id]
+    
+    return closest_hit
+
+def ray_tracing_multi_shape(T, list_shapes):
+    dist = np.inf
+    closest_hit =[np.nan, np.nan, 1.]
+    for shape in list_shapes:
+        current_shape_hit = ray_tracing_single_shape(T, shape)
+        current_dist = np.linalg.norm(current_shape_hit-T[:,2])
+        if(np.isfinite(current_dist) and current_dist < dist):
+            dist = current_dist
+            closest_hit = current_shape_hit
+            
+    return closest_hit
+
+def generate_snake_head():
+    p = np.array([-1., 0., 1.])
+    snake_head = np.vstack([p, 
+                            rigid_tranformation((0, 0, np.pi/3.)) @ p, 
+                            rigid_tranformation((0, 0, (2*np.pi)/3.)) @ p, 
+                            rigid_tranformation((0, 0, np.pi)) @ p,
+                            [1., 1., 1.],
+                            [0.25, 2.5, 1.],
+                            [-0.25, 2.5, 1.],
+                            [-1, 1, 1.]
+                           ]).T
+    snake_head = rigid_tranformation((2., 0., -np.pi/2)) @ snake_head
+
+    snake_hex = np.empty((0,3))
+
+    for theta in np.linspace(0, 2.*np.pi, 7): 
+        snake_hex = np.vstack([snake_hex, rigid_tranformation((0,0,theta)) @ p])
+
+    snake_hex = snake_hex.T
+    snake_hex = rigid_tranformation((0., 0., -np.pi/2)) @ snake_hex
+    snake_head_o = np.array([0,0,1.])
+    
+    return snake_head_o, snake_hex, snake_head
+
+def draw_snake_head(ax, head_parts, T = rigid_tranformation((0,0,0)), draw_frame=False):
+    snake_head_o, snake_hex, snake_head = head_parts
+    
+    poly1 = T @ snake_head
+    poly2 = T @ snake_hex
+    o     = T @ snake_head_o
+    
+    polygone1 = plt.Polygon(poly1[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone1)
+    
+    polygone2 = plt.Polygon(poly2[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone2)
+    
+    if(draw_frame):
+        draw_frame(ax, origin=o[0:2], text_x=r"$\vec{\mathscr{x}}$", color = "tab:blue")
+    all_points = np.hstack([poly1, poly2, o[:,None]])
+    return polygone1, polygone2
+
+
+# generate body
+def generate_snake_body():
+    p = np.array([-1., 0., 1.])
+
+    half_hex = np.vstack([p, 
+                        rigid_tranformation((0, 0, np.pi/3.)) @ p, 
+                        rigid_tranformation((0, 0, (2*np.pi)/3.)) @ p, 
+                        rigid_tranformation((0, 0, np.pi)) @ p
+                         ]).T
+
+    snake_body = np.vstack([half_hex.T,
+                            [1., 1., 1.],
+                            [3., 4., 1.],
+                            (rigid_tranformation((2,5,np.pi)) @ half_hex).T,
+                            [1., 5., 1.],
+                            [1., 4., 1.],
+                            [0., 3., 1.],
+                            [-1., 4., 1.],
+                            [-1., 5., 1.],
+                            (rigid_tranformation((-2,5,np.pi)) @ half_hex).T,
+                            [-3., 4., 1.],
+                            [-1., 1., 1.],
+                           ]).T
+
+    snake_body = rigid_tranformation((0., 0., -np.pi/2)) @ snake_body
+    snake_body_o = np.array([0,0,1.])
+    return snake_body_o, snake_body
+
+def draw_snake_body(ax, body_parts, T = rigid_tranformation((0,0,0)), draw_frame=False):
+    snake_body_o, snake_body = body_parts
+    
+    poly1 = T @ snake_body
+    o     = T @ snake_body_o
+    polygone = plt.Polygon(poly1[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone)
+    if(draw_frame):
+        draw_frame(ax, origin=o[0:2], text_x=r"$\vec{\mathscr{x}}$", color = "tab:blue")
+    all_points = np.hstack([poly1, o[:,None]])
+    return polygone
+
+# Snake tail
+def generate_snake_tail():
+
+    p = np.array([-1., 0., 1.])
+
+    snake_tail1 = np.empty((0,3))
+    for theta in np.linspace(0, 2.*np.pi, 7): 
+        snake_tail1 = np.vstack([snake_tail1, rigid_tranformation((0,0,theta)) @ p])
+
+    snake_tail1 = snake_tail1.T
+
+    snake_tail1 = rigid_tranformation((0., 0., -np.pi/2)) @ snake_tail1
+    snake_tail2 = rigid_tranformation((-2., 0., 0.)) @ snake_tail1
+    snake_tail_o = np.array([0,0,1.])
+    return snake_tail_o, snake_tail1, snake_tail2
+
+def draw_snake_tail(ax, tail_parts, T = rigid_tranformation((0,0,0)), draw_frame=False):
+    snake_tail_o, snake_tail1, snake_tail2 = tail_parts
+    
+    poly1 = T @ snake_tail1
+    poly2 = T @ snake_tail2
+    o     = T @ snake_tail_o
+    
+    polygone1 = plt.Polygon(poly1[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone1)
+    polygone2 = plt.Polygon(poly2[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone2)
+    
+    if(draw_frame):
+        draw_frame(ax, origin=o[0:2], text_x=r"$\vec{\mathscr{x}}$", color = "tab:blue")
+    all_points = np.hstack([poly1, poly2, o[:,None]])
+    return polygone1, polygone2
+
+
+# generate rattle
+def generate_snake_rattle():
+    p = np.array([-1., 0., 1.])
+
+    snake_rattle = np.vstack([p, 
+                          rigid_tranformation((0, 0, np.pi/3.)) @ p, 
+                          rigid_tranformation((0, 0, (2*np.pi)/3.)) @ p, 
+                          rigid_tranformation((0, 0, np.pi)) @ p,
+                          [1.,1.,1.],
+                          [0.,3.,1.],
+                          [-1.,1.,1.]
+                         ]).T
+
+    snake_rattle = rigid_tranformation((0., 0., np.pi/2)) @ snake_rattle
+    
+    snake_rattle_o = np.array([0,0,1.])
+    
+    return snake_rattle_o, snake_rattle
+
+def draw_snake_rattle(ax, rattle_parts, T = rigid_tranformation((0,0,0)), draw_frame=False):
+    snake_rattle_o, snake_rattle = rattle_parts
+    
+    poly1 = T @ snake_rattle
+    o     = T @ snake_rattle_o
+    
+    polygone = plt.Polygon(poly1[0:2,:].T, color='white', alpha=0.2)
+    ax.add_patch(polygone)
+    if(draw_frame):
+        draw_frame(ax, origin=o[0:2], text_x=r"$\vec{\mathscr{x}}$", color = "tab:blue")
+    all_points = np.hstack([poly1, o[:,None]])
+    return polygone
+
+def generate_snake_eye():
+    snake_eye = np.vstack([[0.,-2.,1.],
+                          [1.,0.,1.],
+                          [0.,2.,1.]
+                         ]).T
+    s = 0.3
+    snake_eye = scale_tranformation((s,s)) @ snake_eye
+    
+    snake_eye_o = np.array([0,0,1.])
+    
+    return snake_eye_o, snake_eye
+
+def draw_snake_eye(ax, eye_parts, T=rigid_tranformation((0,0,0)), draw_frame=False):
+    snake_eye_o, snake_eye = eye_parts
+    
+    poly1 = T @ snake_eye
+    o     = T @ snake_eye_o
+    
+    polygone = plt.Polygon(poly1[0:2,:].T, color='red', alpha=0.4)
+    ax.add_patch(polygone)
+    if(draw_frame):
+        draw_frame(ax, origin=o[0:2], text_x=r"$\vec{\mathscr{x}}$", color = "tab:blue")
+    all_points = np.hstack([poly1, o[:,None]])
+    return polygone
+
+
+    
+class SnakeDynamicValidation:
+    def __init__(self, n):
+        self.__x = np.zeros(n)
+        self.__y = np.zeros(n)
+        self.__time = 0
+        
+    def set_position(self, x, y):
+        self.__x[self.__time] = x
+        self.__y[self.__time] = y
+        self.__time += 1
+        
+    def draw_relative_position(self):
+        t = np.arange(len(self.__x))
+        fig = plt.figure(figsize=(12, 6))
+        grid = plt.GridSpec(2, 2, hspace=0.1, wspace=0.2)
+        
+        ax_xy = fig.add_subplot(grid[:, 0])
+        ax_xy.scatter(self.__x, self.__y, alpha=0.4, c=np.arange(len(self.__x)))
+        ax_xy.set_xlabel("X [m]")
+        ax_xy.set_ylabel("Y [m]")
+        
+        ax_xt = fig.add_subplot(grid[0, 1])
+        ax_xt.set_ylabel("X [m]")
+        ax_xt.scatter(t, self.__x, alpha=0.4, c=t, s=5)
+        ax_xt.plot(t, self.__x, alpha=0.4)
+        plt.setp(ax_xt.get_xticklabels(), visible=False)
+        
+        ax_yt = fig.add_subplot(grid[1, 1:], sharex=ax_xt)
+        ax_yt.scatter(t, self.__y, alpha=0.4, c=t, s=5)
+        ax_yt.plot(t, self.__y, alpha=0.4)
+        ax_yt.set_ylabel("Y [m]")
+        ax_yt.set_xlabel("time [s]")
+        
